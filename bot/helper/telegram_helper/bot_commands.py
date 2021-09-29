b5fafb1bfdb3d5271bfa7835a32eac81d6f52099
Juicemirror <89956968+Juicemirror@users.noreply.github.com>
2021-09-29 10:56:05 +0700

    Update bot_commands.py

--- bot/helper/telegram_helper/bot_commands.py
+++ bot/helper/telegram_helper/bot_commands.py
@@ -1,7 +1,7 @@
 class _BotCommands:
     def __init__(self):
         self.StartCommand = 'start'
-        self.MirrorCommand = 'mirror'
+        self.MirrorCommand = 'mirrorin'
         self.UnzipMirrorCommand = 'unzip'
         self.TarMirrorCommand = 'tar'
         self.ZipMirrorCommand = 'zip'
@@ -25,7 +25,7 @@ class _BotCommands:
         self.WatchCommand = 'watch'
         self.TarWatchCommand = 'tarwatch'
         self.ZipWatchCommand = 'zipwatch'
-        self.QbMirrorCommand = 'qbmirror'
+        self.QbMirrorCommand = 'qbmirrorin'
         self.QbUnzipMirrorCommand = 'qbunzip'
         self.QbTarMirrorCommand = 'qbtar'
         self.QbZipMirrorCommand = 'qbzip'
