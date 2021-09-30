e8e8b200cf59aff6a77aecf31abae5b3bb4d8a84
Juicemirror <89956968+Juicemirror@users.noreply.github.com>
2021-09-30 15:15:08 +0700

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -40,6 +40,7 @@ def stats(update, context):
             f'<b>💻 CPU :</b> <code>{cpuUsage}%</code> ' \
             f'<b>🧭 RAM :</b> <code>{memory}%</code> ' \
             f'<b>💿 DISK :</b> <code>{disk}%</code>'
+            f'<b>🤖 Upload By : @sepmirrorleech21_bot%</b>'
     sendMessage(stats, context.bot, update)
 
 
