9fffbdee8668970cc8807fb9a65cb801f6a7a61a
Bagas Firmansyah <89956968+Juicemirror@users.noreply.github.com>
2021-10-02 14:12:22 +0700

    Update gdriveTools.py

--- bot/helper/mirror_utils/upload_utils/gdriveTools.py
+++ bot/helper/mirror_utils/upload_utils/gdriveTools.py
@@ -610,9 +610,9 @@ class GoogleDriveHelper:
                     content += f'<b> | <a href="https://telegra.ph/{self.path[nxt_page]}">Next</a></b>'
                     nxt_page += 1
             Telegraph(access_token=telegraph_token).edit_page(path = self.path[prev_page],
-                                 title = 'Slam Mirrorbot Search',
-                                 author_name='Slam Mirrorbot',
-                                 author_url='https://github.com/SlamDevs/slam-mirrorbot',
+                                 title = 'Search File Bot Sep 21 Publik',
+                                 author_name='Bot Sep 21 Publik - [No Bokep]',
+                                 author_url='https://t.me/sepmirrorleech21_bot',
                                  html_content=content)
         return
 
@@ -807,9 +807,9 @@ class GoogleDriveHelper:
 
         for content in self.telegraph_content :
             self.path.append(Telegraph(access_token=telegraph_token).create_page(
-                                                    title = 'Slam Mirrorbot Search',
-                                                    author_name='Slam Mirrorbot',
-                                                    author_url='https://github.com/SlamDevs/slam-mirrorbot',
+                                                    title = 'Search File Bot Sep 21 Publik',
+                                                    author_name='Bot Sep 21 Publik - [No Bokep]',
+                                                    author_url='https://t.me/sepmirrorleech21_bot',
                                                     html_content=content
                                                     )['path'])
 
