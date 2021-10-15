138aef0e08be31a7a363c78c26c5fd9b1cfcdae0
Bagas Firmansyah <89956968+Juicemirror@users.noreply.github.com>
2021-10-15 21:45:39 +0700

    Update menuxtra.py

--- bot/modules/menuxtra.py
+++ bot/modules/menuxtra.py
@@ -10,14 +10,14 @@ from bot import dispatcher
 def menuxtra(update, context):
     help_string = '''
   ✙ 🧨 *Menu Xtra* 💠 *:*
- ➻ /song: Dapatkan lagu dari youtube
- ➻ /tgm: Upload file ke telegraph
- ➻ /tgt: Upload text ke telegraph
- ➻ /whois: Dapatkan info dari pengguna
- ➻ /tts: Ubah text ke suara
- ➻ /tl: Use /tl LANGUAGE_CODE
- ➻ /ban: Ban pengguna dari grup
- ➻ /jav: Dapatkan Gambar jav idol
+ ➻ /song:Dapatkan lagu dari youtube
+ ➻ /tgm:Upload file ke telegraph
+ ➻ /tgt:Upload text ke telegraph
+ ➻ /whois:Dapatkan info dari pengguna
+ ➻ /tts:Ubah text ke suara
+ ➻ /tl:Use /tl LANGUAGE_CODE
+ ➻ /ban:Ban pengguna dari grup
+ ➻ /jav:Dapatkan Gambar JAV idol
   ═ ═ ═ ═ ═ ═ ═ ═ ═ ═
  '''
     update.effective_message.reply_photo("https://telegra.ph/file/1d77962382170772a14d1.jpg", help_string, parse_mode=ParseMode.MARKDOWN)
