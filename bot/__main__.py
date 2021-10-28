56ed77df76e00cf70ce3e75270ad577214ef87d7
Bagas Firmansyah <89956968+Juicemirror@users.noreply.github.com>
2021-10-28 19:06:27 +0700

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -19,7 +19,7 @@ from bot.helper.telegram_helper.message_utils import *
 from .helper.ext_utils.bot_utils import get_readable_file_size, get_readable_time
 from .helper.telegram_helper.filters import CustomFilters
 from bot.helper.telegram_helper import button_build
-from .modules import authorize, animequotes, ban, cancel_mirror, clone, count, delete, fun, eval, extrahelp, list, leech_settings, jav, jav_strings, mediainfo, mirror, mirror_status, nsfw, nsfwhelp, nhentai, shell, speedtest, stickers, sitesearch, songs, telegraph, text, tts, trt, torrent_search, usage, watch, weebify, whois
+from .modules import authorize, animequotes, ban, cancel_mirror, cat, clone, count, delete, fun, eval, extrahelp, list, leech_settings, jav, jav_strings, mediainfo, mirror, mirror_status, nsfw, nsfwhelp, nhentai, paste, shell, speedtest, stickers, sitesearch, songs, telegraph, text, tts, trt, torrent_search, usage, watch, wife, weebify, whois
 
 format = "%Y %H:%M:%S"
 
