c67ce2a6295486c825e7c858206f0c97d27d8885
Bagas Firmansyah <89956968+Juicemirror@users.noreply.github.com>
2021-10-28 19:21:44 +0700

    Update wife.py

--- bot/modules/wife.py
+++ bot/modules/wife.py
@@ -6,14 +6,14 @@ from pyrogram.types import Message
 from bot import app, dispatcher
 from telegram.ext import CommandHandler
 
-@app.on_message(filters.command(['waifu']))
+@app.on_message(filters.command(['wife']))
 async def waifu(c: Client, m: Message):
 
      http = httpx.AsyncClient(http2=True)
      r = await http.get("https://api.waifu.pics/sfw/waifu")
      rj = r.json()
 
-     await m.reply_photo(rj["url"], caption=f"ini waifumu")
+     await m.reply_photo(rj["url"], caption=f"Ini waifumu 😉")
 
 
 WIFE_HANDLER = CommandHandler("wife", waifu)
