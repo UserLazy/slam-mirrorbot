3a533ba88481294a064c21424b6f2df13dc1b779
Bagas Firmansyah <89956968+Juicemirror@users.noreply.github.com>
2021-10-30 20:07:08 +0700

    Update extrahelp.py

--- bot/modules/extrahelp.py
+++ bot/modules/extrahelp.py
@@ -1,29 +1,26 @@
 import textwrap
-import requests
 
 from telegram import Update, ParseMode
 from telegram.ext import CallbackContext, CommandHandler
+
 
 from bot import dispatcher
 
 def extrahelp(update, context):
     help_string = '''
-   ✙ 🧨 *Menu Xtra* 💠 *:*
- ➻ /song: Get song from youtube
- ➻ /telegraph: Upload file to telegraph
- ➻ /tgt: Upload text to telegraph
- ➻ /whois: get info from user
- ➻ /webss: Upload screenshot from web
- ➻ /tts: Convert text to voice
- ➻ /tl: Use /tl LANGUAGE_CODE
- ➻ /paste: Paste text
- ➻ /ban: Ban user in group
- ➻ /jav: Get jav idol image
- ➻ /cat: Get cat image
+  ✙ *𝐄𝐱𝐭𝐫𝐚* ☠️ *:*
+ ➻ `/song`*:* Get song from youtube
+ ➻ `/tgm`*:* Upload file to telegraph
+ ➻ `/tgt`*:* Upload text to telegraph
+ ➻ `/whois`*:* get info from user
+ ➻ `/webss`*:* Upload screenshot from web
+ ➻ `/tts`*:* Convert text to voice
+ ➻ `/tl`*:* Use /tl [LANGUAGE_CODE]
+ ➻ `/paste`*:* Paste text to pasty
  '''
-    update.effective_message.reply_text(help_string, parse_mode=ParseMode.MARKDOWN)
+    update.effective_message.reply_photo("https://telegra.ph/file/1d77962382170772a14d1.jpg", help_string, parse_mode=ParseMode.MARKDOWN)
 
 
-EXTRAHELP_HANDLER = CommandHandler("extrahelp", extrahelp)
+EXTRAMENU_HANDLER = CommandHandler("extramenu", extrahelp)
 
-dispatcher.add_handler(EXTRAHELP_HANDLER)
+dispatcher.add_handler(EXTRAMENU_HANDLER)
