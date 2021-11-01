c6db63d04f714d1bbf1f7fc198cc8dfbf55585c5
Bagas Firmansyah <89956968+Juicemirror@users.noreply.github.com>
2021-11-01 20:15:06 +0700

    Update whois.py

--- bot/modules/whois.py
+++ bot/modules/whois.py
@@ -24,60 +24,102 @@ async def who_is(client, message):
         return
     if from_user is None:
         await status_message.edit("no valid user_id / message specified")
-        return
-    
-    first_name = from_user.first_name or ""
-    last_name = from_user.last_name or ""
-    username = from_user.username or ""
-    
-    message_out_str = (
-        "<b>👤 Name :</b> "
-        f"<a href='tg://user?id={from_user.id}'>{first_name}</a>\n"
-        f"<b>Suffix:</b> {last_name}\n"
-        f"<b>✅ Username :</b> @{username}\n"
-        f"<b>🌐 User ID :</b> <code>{from_user.id}</code>\n"
-        f"<b>📍 User Link :</b> {from_user.mention}\n" if from_user.username else ""
-        f"<b>Is Deleted:</b> True\n" if from_user.is_deleted else ""
-        f"<b>Is Verified:</b> True" if from_user.is_verified else ""
-        f"<b>Is Scam:</b> True" if from_user.is_scam else ""
-        # f"<b>Is Fake:</b> True" if from_user.is_fake else ""
-        f"<b>Last Seen:</b> <code>{last_online(from_user)}</code>\n\n"
-    )
+       else:
 
-    if message.chat.type in ["supergroup", "channel"]:
-        try:
-            chat_member_p = await message.chat.get_member(from_user.id)
-            joined_date = datetime.fromtimestamp(
-                chat_member_p.joined_date or time.time()
-            ).strftime("%Y.%m.%d %H:%M:%S")
-            message_out_str += (
-                "<b>🛡 Joined on :</b> <code>"
-                f"{joined_date}"
-                "</code>\n"
+        message_out_str = ""
+
+        message_out_str += f"<b>👤 First Name :</b> {from_user.first_name}\n"
+
+        last_name = from_user.last_name or "<b>None</b>"
+
+        message_out_str += f"<b>👤 Last Name :</b> {last_name}\n"
+
+        message_out_str += f"<b>🌐 ID Telegram :</b> <code>{from_user.id}</code>\n"
+
+        username = from_user.username or "<b>None</b>"
+
+        dc_id = from_user.dc_id or "[User Doesnt Have A Valid DP]"
+
+        message_out_str += f"<b>🛡 Data Centre :</b> <code>{dc_id}</code>\n"
+
+        message_out_str += f"<b>⏱ Last Online :</b> {from_user.last_online_date}\n"
+
+        message_out_str += f"<b>👾 User Name :</b> @{username}\n"
+
+        message_out_str += f"<b>🔗 User 𝖫𝗂𝗇𝗄 :</b> <a href='tg://user?id={from_user.id}'><b>Click Disini</b></a>\n"
+
+        if message.chat.type in (("supergroup", "channel")):
+
+            try:
+
+                chat_member_p = await message.chat.get_member(from_user.id)
+
+                joined_date = datetime.fromtimestamp(
+
+                    chat_member_p.joined_date or time.time()
+
+                ).strftime("%d %b %Y at %I:%M %p")
+
+                message_out_str += (
+
+                    "<b>👥 Bergabung Di Grup :</b> <code>"
+
+                    f"{joined_date}"
+
+                    "</code>\n"
+
+                )
+
+            except UserNotParticipant:
+
+                pass
+
+        chat_photo = from_user.photo
+
+        if chat_photo:
+
+            local_user_photo = await client.download_media(
+
+                message=chat_photo.big_file_id
+
             )
-        except UserNotParticipant:
-            pass
-    chat_photo = from_user.photo
-    if chat_photo:
-        local_user_photo = await client.download_media(
-            message=chat_photo.big_file_id
-        )
-        await message.reply_photo(
-            photo=local_user_photo,
-            quote=True,
-            caption=message_out_str,
-            disable_notification=True
-        )
-        os.remove(local_user_photo)
-    else:
-        await message.reply_text(
-            text=message_out_str,
-            quote=True,
-            disable_notification=True
-        )
-    await status_message.delete()
+
+     
+
+            await message.reply_photo(
+
+                photo=local_user_photo,
+
+                quote=True,
+
+                caption=message_out_str,
+
+                parse_mode="html",
+
+                disable_notification=True
+
+            )
+
+            os.remove(local_user_photo)
+
+        else:
+
+            await message.reply_text(
+
+                text=message_out_str,
+
+                quote=True,
+
+                parse_mode="html",
+
+                disable_notification=True
+
+            )
+
+        await status_message.delete()
 
 
 WHOIS_HANDLER = CommandHandler("whois", who_is)
+
 
 dispatcher.add_handler(WHOIS_HANDLER)
