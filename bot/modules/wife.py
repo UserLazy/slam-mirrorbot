e8aa3994b3f9046ab7d83ab15b1fe323798fec53
Bagas Firmansyah <89956968+Juicemirror@users.noreply.github.com>
2021-11-01 23:14:31 +0700

    Update wife.py

--- bot/modules/wife.py
+++ bot/modules/wife.py
@@ -13,7 +13,7 @@ async def waifu(c: Client, m: Message):
      r = await http.get("https://api.waifu.pics/sfw/waifu")
      rj = r.json()
 
-     await m.reply_photo(rj["url"], caption=@{message.from_user.username}f"Ini waifumu 💙💙💙💙💙")
+     await m.reply_photo(rj["url"], caption=@{m.from_user.username}f"Ini waifumu 💙💙💙💙💙")
 
 
 WIFE_HANDLER = CommandHandler("wife", waifu)
