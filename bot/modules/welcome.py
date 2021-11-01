612c930ec4f18c4f8840ed60f9e71a31feef10af
Bagas Firmansyah <89956968+Juicemirror@users.noreply.github.com>
2021-11-02 00:17:57 +0700

    Update welcome.py

--- bot/modules/welcome.py
+++ bot/modules/welcome.py
@@ -1,20 +1,39 @@
-import os
-from pyrogram import Client, filters
-from pyrogram.types import Message, User, InlineKeyboardButton, InlineKeyboardMarkup
-from bot import app
-
-
-@app.on_message(filters.new_chat_members)
-async def welcome(bot,message):
-	chatid = message.chat.id
-	markup = InlineKeyboardMarkup(
-        [[
-        InlineKeyboardButton('⚙ Contact Owner ⚙', url='https://telegram.me/OdierBambi')
-        ]]
-    )
-	await bot.send_message(text=f"<b> Hallo {message.from_user.mention}\n Your ID: <code>{message.from_user.id}</code>\n Selamat Datang Di Grup {message.chat.title}\n\n Klik /cmdhelp untuk menampilkan menu perintah</b>", chat_id=chatid, reply_markup=markup)
-	
-@app.on_message(filters.left_chat_member)
-async def goodbye(bot,message):
-	chatid= message.chat.id
-	await bot.send_message(text=f"<b> Selamat jalan\n {message.from_user.mention}\n Semoga harimu menyenangkan😁</b>", chat_id=chatid)
+import os
+
+from pyrogram import Client, filters
+
+from pyrogram.types import Message, User, InlineKeyboardButton, InlineKeyboardMarkup
+
+from bot import app
+
+
+
+
+
+@app.on_message(filters.new_chat_members)
+
+async def welcome(bot,message):
+
+	chatid = message.chat.id
+
+	markup = InlineKeyboardMarkup(
+
+        [[
+
+        InlineKeyboardButton('🤖 Contact Owner Jika Ingin Berdonasi 👤', url='https://t.me/uzumakinaruto4backup_bot')
+
+        ]]
+
+    )
+
+	await bot.send_message(text=f"<b> Hai,Apa Kabar? {message.from_user.mention}\n\n Your ID : <code>{message.from_user.id}</code>\n\n 👥 Selamat Datang Di Grup , Klik /rules u/ melihat peraturan grup {message.chat.title}\n\n Klik /help untuk menampilkan menu perintah</b>", chat_id=chatid, reply_markup=markup)
+
+	
+
+@app.on_message(filters.left_chat_member)
+
+async def goodbye(bot,message):
+
+	chatid= message.chat.id
+
+	await bot.send_message(text=f"<b> Hati-hati di jalan,titip gorengan & es teh\n\n {message.from_user.mention}\n\n ⏰ Jangan balik lagi 🧨</b>", chat_id=chatid)
