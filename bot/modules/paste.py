6d24d1e82bd1bdc86472a980ca15aebea97bd226
Bagas Firmansyah <89956968+Juicemirror@users.noreply.github.com>
2021-11-03 13:25:20 +0700

    Update paste.py

--- bot/modules/paste.py
+++ bot/modules/paste.py
@@ -21,7 +21,12 @@ async def hastebin(c: Client, m: Message):
         url = "https://hastebin.com/documents"
         r = await http.post(url, data=mean.encode("UTF-8"))
         url = f"https://hastebin.com/{r.json()['key']}"
-        await m.reply_text(url, disable_web_page_preview=True)
+        markup = InlineKeyboardMarkup(
+        [[
+        InlineKeyboardButton('📝 Paste Link', url=url)
+        ]]
+    )
+        await m.reply_text(url, disable_web_page_preview=False)
     else:
         await m.reply_text("reply_to_document_or_text")
 
